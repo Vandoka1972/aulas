878881047f7d1f02543cedc5f8c8c0e17920a49c
Vandoka1972 <vandomaja72@gmail.com>
2021-07-02 02:16:58 -0300

    Add Cards Prices

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,13 +1,32 @@
 
+import { useState, useEffect  } from 'react';
+import { fetchProducts } from '../api';
+import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
 import './styles.css';
-
+import { Product } from './types';
 
 
 function Orders(){
+
+    const [products, setProducts] = useState<Product[]>([]);
+    //console.log(products);
+
+
+
+    useEffect(() => {
+     //console.log('componente orders iniciou')
+     fetchProducts()
+       .then(response => setProducts(response.data))
+       .catch(error => console.log(error))
+
+    }, []);
+
+
     return(
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products = {products}/>
         </div>
     )
 }
